f185524ce7db48f05c66012a4b6e8312a57d05dd
Luiz Henrique Saydt <luiz.saydt@icolabora.com>
2023-12-23 17:58:02 -0300

    Adicionado import manual para o intelisense do TypeScript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // eslint-disable-next-line no-undef
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
   ssr: false,
   spaLoadingTemplate: "spa-loading-template.html",
