b5a5afe52aae3333ab2d05f19394e21320aa7902
Luiz Henrique Saydt <luiz.saydt@icolabora.com>
2023-12-23 18:18:15 -0300

    Rotas

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,16 @@ import Route from "@ioc:Adonis/Core/Route";
 Route.get("/", async () => {
   return { hello: "world" };
 });
+
+Route.get("/auth", "AuthController.check");
+Route.post("/auth/sign-in", "AuthController.signIn");
+Route.post("/auth/sign-up", "AuthController.signUp");
+Route.post("/auth/sign-out", "AuthController.signOut");
+
+Route.get("favorite", "FavoritesController.all");
+Route.post("favorite/:id", "FavoritesController.favoriteDrink");
+Route.patch("favorite/:id", "FavoritesController.unFavoriteDrink");
+
+Route.get("drinks", "DrinksController.all");
+Route.get("drink-image/:id", "DrinksController.image");
+Route.get("drink-image-base64/:id", "DrinksController.imageBase64");
